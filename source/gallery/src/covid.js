b3858a1f63d12e348496d19d5a211d7c0b6577ab
Dan O'Neill <dev@jdoneill.com>
2020-03-23 16:51:45 -0700

    update attribution & color ramp

--- source/gallery/src/covid.js
+++ source/gallery/src/covid.js
@@ -1,13 +1,17 @@
+mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+geojsonLink = '<a href="https://www.worldometers.info/coronavirus/country/us/">worldometer</a>';
+
 var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href=”http://osm.org/copyright”>OpenStreetMap</a> contributors',
+    attribution: '&copy; ' + mapLink + ' Contributors',
     maxzoom: 18
 });
 
 function setStateColor(d) {
     return d > 5000 ? '#bd0026' :
-      d > 1000 ? '#f03b20' :
-      d > 500 ? '#fd8d3c' :
-      d > 100 ? '#feb24c' :
+      d > 2500 ? '#f03b20' :
+      d > 1000 ? '#fd8d3c' :
+      d > 500 ? '#feb24c' :
+      d > 100 ? '#fed976' :
       '#ffffb2';
   }
 
@@ -28,6 +32,8 @@ $.getJSON("./data/states.geojson", function(data) {
         layer.bindPopup('<b>Name: </b>'+feature.properties.NAME+'<br><b>Total Cases: </b>'+feature.properties.COVID_CONFIRMED+'<br><b>New Cases: </b>'+feature.properties.COVID_NEW+'<br><b>Total Deaths: </b>'+feature.properties.COVID_DEATHS);
       }
     });
+
+    geojson.getAttribution = function() { return geojsonLink; };
 
     var map = L.map('mapid').setView([39.828329, -98.579453], 4);
     tiles.addTo(map);
