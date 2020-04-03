86cfe5d87c9b0c3eaf80e4f8599d492e61bee403
Dan O'Neill <dev@jdoneill.com>
2020-04-02 21:18:14 -0700

    pull county data from esri service
    parse confirmed and death values into counties polygon geojson

--- wa-counties-covid.py
+++ wa-counties-covid.py
@@ -1,70 +1,26 @@
 import json
-import requests
-import lxml.html as lh
-import pandas as pd
-from htmlmin import minify
 
-# ///////////////////////////////////////////////////
-# scrape url table and convert to panda datatable
-# ///////////////////////////////////////////////////
+with open('./source/gallery/data/counties-wa.geojson') as g:
+  counties = json.load(g)
 
-url = 'https://www.doh.wa.gov/Emergencies/Coronavirus/TSPD'
-page = requests.get(url)
-doc = lh.fromstring(page.content)
-tr_elements = doc.xpath('//tr')
+with open('./source/gallery/data/counties-wa-point.geojson') as f:
+  features = json.load(f)
 
-col = []
-i = 0
-
-for header in tr_elements[0]:
-  i+=1
-  name = header.text_content()
-  col.append((name, []))
-
-for j in range(1, len(tr_elements)):
-  tr_element = tr_elements[j]
-
-  i = 0
-  for row in tr_element.iterchildren():
-    data = row.text_content()
-    min_data = minify(data)
-    value = min_data.replace(',', '')
-    if i > 0:
-      try:
-        data = int(value)
-      except:
-        pass
-
-    col[i][1].append(value)
-    i+=1
-
-d = {title:column for (title,column) in col}
-df = pd.DataFrame(d)
-
-# ///////////////////////////////////////////////////
-# update states.geojson with covid data from datatable
-# ///////////////////////////////////////////////////
-
-with open('./source/gallery/data/counties-wa.geojson') as f:
-  geojson = json.load(f)
-
-for i in df.index:
-  if i < 34:
-    for feature in geojson['features']:
-      countyName =  feature['properties']['NAME']
-      datacountyName = str.strip(df['County'][i])
-
-      if (countyName == datacountyName):
-        totalCases = df['Positive/Confirmed Cases'][i]
-        totalDeaths = df['Deaths'][i]
-        feature['properties']['COVID_CONFIRMED'] = totalCases
-        feature['properties']['COVID_DEATHS'] = totalDeaths
-
-    # print out results
-    print(datacountyName, totalCases, totalDeaths)
+for county in counties['features']:
+  name = county['properties']['NAME']
+  for feature in features:
+    admin2 = feature['properties']['Admin2']
+    confirmed = feature['properties']['Confirmed']
+    deaths = feature['properties']['Deaths']
+    if (name == admin2):
+      county['properties']['COVID_CONFIRMED'] = confirmed
+      county['properties']['COVID_DEATHS'] = deaths
+    
+      # print out results
+      print(name, county['properties']['COVID_CONFIRMED'], county['properties']['COVID_DEATHS'])
 
 with open('./source/gallery/data/counties-wa.geojson', 'w') as f:
   f.seek(0)
-  json.dump(geojson, f, indent=2)
+  json.dump(counties, f, indent=2)
   f.truncate()
 f.close
---
+++ wa-counties-covid.sh
@@ -0,0 +1,7 @@
+ #!/bin/bash 
+
+# pipe result to geojson file
+curl 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/ncov_cases_US/FeatureServer/0/query?where=1%3D1&outFields=*&f=geojson&token=' | jq '.features[] | select(.properties.Province_State=="Washington")' > temp.geojson
+# append comma after each feature
+jq -s . temp.geojson > ./source/gallery/data/counties-wa-point.geojson
+rm temp.geojson
