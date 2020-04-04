919ebd788988f7b83de7d5ee7556bff50936ea00
Dan O'Neill <dev@jdoneill.com>
2020-04-03 17:18:41 -0700

    format updated esri time and commit to counties geojson

--- wa-counties-covid.py
+++ wa-counties-covid.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 with open('./source/gallery/data/counties-wa.geojson') as g:
   counties = json.load(g)
@@ -9,15 +10,19 @@ with open('./source/gallery/data/counties-wa-point.geojson') as f:
 for county in counties['features']:
   name = county['properties']['NAME']
   for feature in features:
+    esriFieldTypeDate = feature['properties']['Last_Update']
+    datetimeVal = esriFieldTypeDate / 1000.0
+    timestamp = datetime.datetime.fromtimestamp(datetimeVal).strftime('%Y-%m-%d %H:%M:%S')
     admin2 = feature['properties']['Admin2']
     confirmed = feature['properties']['Confirmed']
     deaths = feature['properties']['Deaths']
     if (name == admin2):
       county['properties']['COVID_CONFIRMED'] = confirmed
       county['properties']['COVID_DEATHS'] = deaths
+      county['properties']['UPDATED'] = timestamp
     
       # print out results
-      print(name, county['properties']['COVID_CONFIRMED'], county['properties']['COVID_DEATHS'])
+      print(name, county['properties']['COVID_CONFIRMED'], county['properties']['COVID_DEATHS'], county['properties']['UPDATED'])
 
 with open('./source/gallery/data/counties-wa.geojson', 'w') as f:
   f.seek(0)
