bbb8100a537046b0737436ef8adec633bd4f7ee9
Dan O'Neill <dev@jdoneill.com>
2020-04-04 14:12:39 -0700

    style by area

--- source/gallery/src/seattle-json.js
+++ source/gallery/src/seattle-json.js
@@ -2,18 +2,33 @@ mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 
 var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; ' + mapLink + ' Contributors',
-    maxzoom: 18
+    minZoom: 8,
+    maxZoom: 18
 });
 
-var geojsonStyle = {
-    "color": "#6E6E6E",
+function getColor(d) {
+  return d > 300000000 ? '#4682B4' :
+      d > 250000000  ? '#6495ED' :
+      d > 100000000  ? '#1E90FF' :
+      d > 50000000  ? '#B0C4DE' :
+      d > 10000000   ? '#00BFFF' :
+      d > 7500000   ? '#87CEEB' :
+      d > 5000000   ? '#87CEFA' :
+            '#ADD8E6';
+}
+
+function setStyle(feature) {
+  return {
+    fillColor: getColor(feature.properties.area),
     "weight": 1,
-    "opacity": 0.55
-};
+    fillOpacity: 0.7,
+    "opacity": 0.80
+  };
+}
 
 $.getJSON("./data/seattle-neighborhoods.geojson", function(data) {
     var geojson = L.geoJson(data, {
-        style: geojsonStyle,
+        style: setStyle,
       onEachFeature: function (feature, layer) {
         layer.bindPopup('<b>Name: </b>'+feature.properties.name+'<br><b>Area: </b>'+feature.properties.area);
       }
