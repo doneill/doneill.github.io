35b029de667271d4e649cb0924f1934c5f96d56b
Dan O'Neill <dev@jdoneill.com>
2021-05-17 22:01:29 -0700

    update py env

--- update-data.sh
+++ update-data.sh
@@ -1,4 +1,4 @@
- #!/bin/bash 
+ #!/bin/bash
 
 # pipe result to geojson file
 curl 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/ncov_cases_US/FeatureServer/0/query?where=1%3D1&outFields=*&f=geojson&token=' | jq '.features[] | select(.properties.Province_State=="Washington")' > temp.geojson
@@ -7,5 +7,5 @@ jq -s . temp.geojson > ./source/gallery/data/counties-wa-point.geojson
 rm temp.geojson
 
 # update geojson data
-python3 wa-counties-covid.py
-python3 covid19.py
+pipenv run python wa-counties-covid.py
+pipenv run python covid19.py
